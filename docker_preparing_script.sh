19c8c35304933e30a28ad2f07adc95d056b687c4
Константин В. Соловьёв <kvsolovev@yoomoney.ru>
2024-03-24 11:14:06 +0300

    docker script fix

--- docker_preparing_script.sh
+++ docker_preparing_script.sh
@@ -3,6 +3,10 @@
 echo "### Stop running docker-compose ###"
 docker-compose down
 
+echo "### Stop and remove  containers ###"
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+
 if [ "$1" = "push" ]; then
   echo "### Build & push images ###"
   bash ./gradlew  :rococo-auth:jib || exit 1
